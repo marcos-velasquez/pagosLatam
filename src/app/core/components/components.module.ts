8c9629edfd6be4f920ba79910ada2aff57a9cfb7
Marcos Velasquez <marcos.svz28@gmail.com>
2022-01-11 01:40:01 -0400

    footer component

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -7,10 +7,11 @@ import { FormsModule } from '@angular/forms';
 import { PickListComponent } from './pick-list/pick-list.component';
 import { InputPhoneComponent } from './input-phone/input-phone.component';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, PrimeNgModule, FormsModule, NgxIntlTelInputModule],
-  exports: [PrimeNgModule, ToolbarComponent, PickListComponent, InputPhoneComponent],
-  declarations: [ToolbarComponent, SidebarComponent, PickListComponent, InputPhoneComponent],
+  exports: [PrimeNgModule, ToolbarComponent, PickListComponent, InputPhoneComponent, FooterComponent],
+  declarations: [ToolbarComponent, SidebarComponent, PickListComponent, InputPhoneComponent, FooterComponent],
 })
 export class ComponentsModule {}
