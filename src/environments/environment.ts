e3e88fd7713a113128e1bc8ab85a20a7eb4231b9
Marcos Velasquez <marcos.svz28@gmail.com>
2022-01-13 22:35:17 -0400

    hidden firebase credential

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDiRlD4CHN7tI_XCtZdJfo_hblEbUc6Pes',
-    authDomain: 'pagos-latam.firebaseapp.com',
-    projectId: 'pagos-latam',
-    storageBucket: 'pagos-latam.appspot.com',
-    messagingSenderId: '1050808149282',
-    appId: '1:1050808149282:web:65334bee8952197b95b17b',
-    measurementId: 'G-BSLXT4GYN1',
+    apiKey: '*****',
+    authDomain: '*****',
+    projectId: '*****',
+    storageBucket: '*****',
+    messagingSenderId: '*****',
+    appId: '*****',
+    measurementId: '*****',
   },
 };
 
