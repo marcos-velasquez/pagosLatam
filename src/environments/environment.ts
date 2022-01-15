dc896ff9587b6cf4b5be2d57b87f560892d98093
Marcos Velasquez <marcos.svz28@gmail.com>
2022-01-15 09:51:10 -0400

    history table responsive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
+  /*   firebaseConfig: {
     apiKey: '*****',
     authDomain: '*****',
     projectId: '*****',
@@ -12,6 +12,15 @@ export const environment = {
     messagingSenderId: '*****',
     appId: '*****',
     measurementId: '*****',
+  }, */
+  firebaseConfig: {
+    apiKey: 'AIzaSyDiRlD4CHN7tI_XCtZdJfo_hblEbUc6Pes',
+    authDomain: 'pagos-latam.firebaseapp.com',
+    projectId: 'pagos-latam',
+    storageBucket: 'pagos-latam.appspot.com',
+    messagingSenderId: '1050808149282',
+    appId: '1:1050808149282:web:65334bee8952197b95b17b',
+    measurementId: 'G-BSLXT4GYN1',
   },
 };
 
